8953132a22d81578f15cddaa424cdf161940455d
chaehyeo-n <parkche03@naver.com>
2024-10-01 18:40:55 +0900

    Fix: nginx 환경 css 설정을 위한 settings.py 수정

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
