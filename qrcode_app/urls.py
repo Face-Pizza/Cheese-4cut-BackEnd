c810578e61509f211b7de0b1a935e20c5cb9a475
chaehyeo-n <parkche03@naver.com>
2024-10-04 23:16:59 +0900

    Fix: url 암호화 이전 버전 복구

--- qrcode_app/urls.py
+++ qrcode_app/urls.py
@@ -2,12 +2,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
-from .views import PhotoViewSet, qr_code_view
+from .views import PhotoViewSet
 
 router = DefaultRouter()
 router.register(r'photos', PhotoViewSet, basename='photo')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('qr/<str:encrypted_id>/', qr_code_view, name='qr_code_view'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
